53e911f383a4b9ee7f67bd99f6d46239c781bb63
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-06-11 20:11:49 +0100

    create review page

--- src/pages/review-page/CreateReview.tsx
+++ src/pages/review-page/CreateReview.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router';
+import './CreateReview.css'
+import Rate from '../../components/icons/Rate';
 
 const CreateReview = () => {
+
+  const location = useLocation();
+  const poster = location.state && location.state.poster;
+  const filmTitle = location.state && location.state.filmTitle;
+  const year = location.state && location.state.year;
+  const username = "testing1"
+
+
+  useEffect(() => {
+    console.log("year ", year)
+  })
+  
   return (
-    <div>CreateReview</div>
+    <div className="create-review">
+       <div className="create-review-poster">
+        <img src={poster} alt="film-poster" />
+      </div>
+      
+      <div className="create-review-forum">
+        <div className="create-review-heading">
+          <h3><b>A review by {username}</b></h3>
+          <h5>Film: {filmTitle} ({year})</h5>
+          
+          </div>
+      </div>
+      <div className="create-review-rating">
+        <Rate inCreatePage={true}/>
+      </div>
+
+
+
+
+    </div>
   )
 }
 
