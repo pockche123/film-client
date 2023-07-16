6b8a57159fa5ce4eb6e52eaf2edcb691d1b15972
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-07-16 20:39:00 +0100

    changes

--- src/components/images/ImageBlock.tsx
+++ src/components/images/ImageBlock.tsx
@@ -3,18 +3,34 @@ import './ImageBlock.css'
 import { Film } from '../../interfaces/IFilm'
 
 const ImageBlock = ({ images }: { images: string[] }) => {
+
+
+  const handleImages = () =>{
+    
+  }
   return (
     <div className='image-block'>
+      <div className="image-head">
       <h4
-        style={{ marginLeft: '20px', marginRight: '40px', textAlign: 'left' }}
+        id="image-title"
       >
-        Images
-        <div className="image">
-          {images.map(image => (
-            <img src={image} alt='backdrop' id="backdrop" />
-          ))}
-        </div>
-      </h4>
+        Images 
+        </h4>
+        <span id="image-handle">
+        <b onClick={handleImages}>
+          Go to all images
+          </b>
+          </span>
+
+
+      </div>
+
+      <div className='image'>
+  {images.map(image => (
+    <img src={image} alt='backdrop' id='backdrop' />
+  ))}
+</div>
+
     </div>
   )
 }
