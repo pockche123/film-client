0761229f631f70c1b12cef804f7906dff63e616e
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-09-05 18:09:02 +0100

    block created

---
+++ src/pages/user-pages/profile-page/ReportOptions.ts
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const ReportOptions = [
+    "Please select a reason...",
+    "Account exhibits a pattern of posting unwelcome, aggressive or abusive remarks directed at another member",
+    "Account exhibits a pattern of posting racist, sexist, homophobic or other discriminatory views",
+    "Account exhibits a pattern of plagiarism (please include link/s to original content)",
+    "Account promotes piracy or other illegal activity", 
+    "Account is attempting to manipulate film ratings or popularity (by following an excessive number of accounts",
+    "Account is posting unsolicited links to content, products or services, including for self-promotion",
+    "Account is an impersonation, satire or parody",
+    "Account has attempted to solicit personal information from a member (please include a relevant link)",
+    "Account has an offensive username, avatar or bio",
+    "Other Reason"
+]
+
+export default ReportOptions
