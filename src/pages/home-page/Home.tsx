a3bbdcd2cddc7a0553beaf6aec6390b24ff6eaf6
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-09-29 18:14:01 +0100

    api works

--- src/pages/home-page/Home.tsx
+++ src/pages/home-page/Home.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Films from '../../components/films/Films'
 import LandingPage from '../../components/landing/LandingPage'
 import { Navbar } from 'react-bootstrap'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
+import { getTopRatedFilms } from '../../components/services/API/Films'
 
 const Home = () => {
+
+  const [data, setData] = useState(); 
+  
+  useEffect(() => {
+
+    getTopRatedFilms()
+      .then(res => setData(res.data))
+     
+    
+  
+    console.log("THIS IS THE TOP RATED FILMS ",  data);
+})
+
+
   return (
     <>
       <Header/>
